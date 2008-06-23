2cd4e866a982606b697e54f8a661c5df9e970a8c
Max Howell <max@methylblue.com>
2008-06-23 16:47:59 +0000

    pre-renamings commit.
    git-svn-id: svn+ssh://svn.last.fm/svn/misc/max/v2@84597 ab8f4a45-97f9-0310-bbd1-854ce3dcee89

--- src/app/scrobbler/ScrobbleShepherd.cpp
+++ src/app/scrobbler/ScrobbleShepherd.cpp
@@ -477,7 +477,7 @@ ScrobblerHttpPostRequest::request()
     header.setValue( "Host", host() ); //Qt makes me LOL today
     header.setContentType( "application/x-www-form-urlencoded" );
 
-    qDebug() << "HTTP POST" << host() + '/' + m_path << m_data;
+    qDebug() << "HTTP POST" << host() + m_path << m_data;
 
     m_id = QHttp::request( header, m_data );
 }
