f6abd8541b8ea25421a412e7aad6839f0b03056b
Max Howell <max@methylblue.com>
2008-06-23 18:32:21 +0000

    svn:ignore some generate build system script changes
    git-svn-id: svn+ssh://svn.last.fm/svn/misc/max/v2@84651 ab8f4a45-97f9-0310-bbd1-854ce3dcee89

--- generate_ide_project.sh
+++ generate_ide_project.sh
@@ -4,13 +4,14 @@ windows()
 {
     # we step into src because qmake is b0rked
     cd src
-    qmake -recursive -tp vc moose.pro
+    qmake -recursive -tp vc pigmyshrew.pro
 
     AFTER='
 Project("{2150E333-8FDC-42A3-9474-1A3956D46DE8}") = "Solution Items", "Solution Items", "{780C13D3-8172-4EE0-8FD5-26ED6489851C}"
 	ProjectSection(SolutionItems) = preProject
 		include.pro = include.pro
 		.qmake.cache = .qmake.cache
+		pigmyshrew.pro = pigmyshrew.pro
 	EndProjectSection
 EndProject
 Project("{2150E333-8FDC-42A3-9474-1A3956D46DE8}") = "common", "common", "{3FDD67B7-DF67-4F22-8545-755D722794BC}"
@@ -20,7 +21,9 @@ Project("{2150E333-8FDC-42A3-9474-1A3956D46DE8}") = "common", "common", "{3FDD67
 EndProject
 '
 
-    echo "$AFTER" >> moose.sln
+    echo "$AFTER" >> pigmyshrew.sln
+
+    rmdir _build
 }
 
 osx() 
