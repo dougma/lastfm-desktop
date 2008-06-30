c788d3313b3d15e8e6c02ebfd6ce7423faa95ed3
Adam Renburg <adam@last.fm>
2008-06-30 16:35:49 +0000

    prints if errors to console
    git-svn-id: svn+ssh://svn.last.fm/svn/misc/max/v2@85976 ab8f4a45-97f9-0310-bbd1-854ce3dcee89

--- tests/logTest.sh
+++ tests/logTest.sh
@@ -2,6 +2,7 @@
 
 RUNDIR=`dirname $0`
 
+TOTERRORS=0
 for i in $*
 do
   BINDIR=`dirname $i`
@@ -11,5 +12,19 @@ do
   OUTPUTFILE=$RUNDIR/output/$OUTPUTDIR.output
   
   $i  -xml > "$OUTPUTFILE"
+  
+  NUMERRORS=`grep -c \"fail\" $OUTPUTFILE`
+  let TOTERRORS=TOTERRORS+NUMERRORS
+  
+  if [[ NUMERRORS -gt 0 ]];
+  then
+    echo "$NUMERRORS errors where encountered in $i"
+  fi
+  
   xsltproc $RUNDIR/QtTest_to_JUnit.xslt $OUTPUTFILE > "$OUTPUTFILE.junit.xml"
 done
+
+if [[ TOTERRORS -gt 0 ]];
+then
+  echo "Total: $TOTERRORS errors"
+fi
