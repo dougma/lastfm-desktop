c62d40d274d009ac721ed79adc3df8b182068c42
Max Howell <max@methylblue.com>
2008-08-22 16:58:35 +0000

    * Fixes some radio behaviour * You now have to provide your own audiooutput to the radio, this makes sense for us, since we need to configure it from the app layer, but also for other projects that may use it * Removed AudioPlaybackEngine as it just became a light wrapper around media object, and it was already radio specific
    git-svn-id: svn+ssh://svn.last.fm/svn/clientside/trunk/desktop@97585 ab8f4a45-97f9-0310-bbd1-854ce3dcee89

--- lib/radio/RadioController.cpp
+++ lib/radio/RadioController.cpp
@@ -1,17 +1,37 @@
+/***************************************************************************
+ *   Copyright 2005-2008 Last.fm Ltd.                                      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Steet, Fifth Floor, Boston, MA  02110-1301, USA.          *
+ ***************************************************************************/
+
 #include "RadioController.h"
 #include "Tuner.h"
-#include "AudioPlaybackEngine.h"
+#include "phonon"
 
 
-RadioController::RadioController()
-			   : m_tuner( 0 )
+RadioController::RadioController( Phonon::AudioOutput* output )
+			   : m_tuner( 0 ),
+			     m_audioOutput( output )
 {
-    m_audio = new AudioPlaybackEngine( this );
-    
-    connect( m_audio, SIGNAL(trackStarted( Track )), SIGNAL(trackStarted( Track )) );
-    connect( m_audio, SIGNAL(playbackEnded()), SIGNAL(playbackEnded()) );
-    connect( m_audio, SIGNAL(buffering()), SIGNAL(buffering()) );
-    connect( m_audio, SIGNAL(finishedBuffering()), SIGNAL(playbackResumed()) );
+    m_mediaObject = new Phonon::MediaObject( this );
+    m_mediaObject->setTickInterval( 1000 );
+    connect( m_mediaObject, SIGNAL(stateChanged( Phonon::State, Phonon::State )), SLOT(onPhononStateChanged( Phonon::State, Phonon::State )) );
+	connect( m_mediaObject, SIGNAL(aboutToFinish()), SLOT(onPhononAboutToFinish()) );
+	connect( m_mediaObject, SIGNAL(bufferStatus( int )), SIGNAL(buffering( int )) );
+    Phonon::createPath( m_mediaObject, m_audioOutput );
 }
 
 
@@ -20,35 +40,157 @@ RadioController::play( const RadioStation& station )
 {
 	qInfo() << "Tuning to:" << station;
 	
-	m_audio->stop();
+	stop();
 	delete m_tuner;
-	
+
 	emit tuningIn( station );
     m_tuner = new Tuner( station );
 	connect( m_tuner, SIGNAL(stationName( QString )), SIGNAL(tuned( QString )) );
 	connect( m_tuner, SIGNAL(tracks( QList<Track> )), SLOT(enqueue( QList<Track> )) );
-
-    connect( m_audio, SIGNAL(queueStarved()), m_tuner, SLOT(fetchFiveMoreTracks()) );
 }
 
 
 void
 RadioController::enqueue( const QList<Track>& tracks )
 {
-    m_audio->queue( tracks );
-    m_audio->play();
+    QList<QUrl> urls;
+    foreach (const Track& t, tracks)
+    {
+        urls += t.url();
+        m_queue[t.url()] = t;
+    }
+	
+    m_mediaObject->enqueue( urls );
+	m_mediaObject->play();
+}
+
+
+void
+RadioController::skip()
+{
+	QList<Phonon::MediaSource> q = m_mediaObject->queue();
+	if (q.size())
+	{
+		Phonon::MediaSource source = q.front();
+		q.pop_front();
+		m_mediaObject->setQueue( q );
+		m_mediaObject->setCurrentSource( source );
+		m_mediaObject->play();
+	}
+	//else we already asked for more tracks, so wait I guess
 }
 
 
 void
 RadioController::stop()
 {
-    m_audio->stop();
+	m_mediaObject->stop();
+	m_mediaObject->clearQueue();
+	m_mediaObject->setCurrentSource( QUrl() );
 }
 
 
-void 
-RadioController::skip()
+
+void
+RadioController::pause()
 {
-    m_audio->skip();
+	m_mediaObject->pause();
+}
+
+
+void
+RadioController::unpause()
+{
+	m_mediaObject->play();
+}
+
+
+
+namespace Phonon
+{
+	static inline QString debug( int state )
+	{
+	#define _( x ) x: return #x;
+		switch (state)
+		{
+			case _(Phonon::LoadingState)
+			case _(Phonon::StoppedState)
+			case _(Phonon::PlayingState)
+			case _(Phonon::BufferingState)
+			case _(Phonon::PausedState)
+			case _(Phonon::ErrorState)
+		}
+	#undef _
+	}
+	
+	void debug( Phonon::State newstate, Phonon::State oldstate )
+	{
+		qDebug() << "Now is" << debug( newstate ) << "but was" << debug( oldstate );
+	}
+}
+
+
+void
+RadioController::onPhononStateChanged( Phonon::State newstate, Phonon::State oldstate )
+{
+	Phonon::debug( newstate, oldstate );
+	
+	QUrl const url = m_mediaObject->currentSource().url();
+	
+    switch (newstate)
+    {
+        case Phonon::ErrorState:
+            qCritical() << m_mediaObject->errorString();
+            break;
+			
+        case Phonon::StoppedState:
+			emit playbackEnded();
+            break;
+			
+        case Phonon::BufferingState:
+            emit buffering( 0 );
+            break;
+			
+		case Phonon::PausedState:
+			emit playbackPaused();
+			break;
+			
+		case Phonon::LoadingState:
+			emit preparing( m_queue[url] );
+			break;
+			
+        case Phonon::PlayingState:
+			switch (oldstate)
+			{
+				default:
+				{
+					qDebug() << "PlaybackStarted:" << url;
+					
+					Track t = m_queue.take( url );
+					if( t.isEmpty() )
+						break;
+					
+					MutableTrack( t ).stamp();
+					
+					emit trackStarted( t );
+					
+					if (m_queue.isEmpty() && m_tuner)
+						m_tuner->fetchFiveMoreTracks();
+				}
+				break;
+
+				case Phonon::PausedState:
+				case Phonon::BufferingState:
+					emit playbackResumed();
+					break;
+			}
+			break;
+    }
+}
+
+
+void
+RadioController::onPhononAboutToFinish()
+{
+//	emit playbackEnded();
 }
--- lib/radio/RadioController.h
+++ lib/radio/RadioController.h
@@ -1,14 +1,37 @@
+/***************************************************************************
+ *   Copyright 2005-2008 Last.fm Ltd.                                      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Steet, Fifth Floor, Boston, MA  02110-1301, USA.          *
+ ***************************************************************************/
+
 #ifndef RADIOCONTROLLER_H
 #define RADIOCONTROLLER_H
 
 #include <QList>
 #include <QObject>
 #include "lib/types/Track.h"
+#include "lib/radio/RadioStation.h"
 #include "lib/DllExportMacro.h"
-class AudioPlaybackEngine;
-class RadioStation;
-class Tuner;
-class Track;
+#include "phononnamespace.h"
+namespace Phonon
+{
+	class MediaObject;
+	class AudioOutput;
+	class MediaSource;
+}
 
 
 class RADIO_DLLEXPORT RadioController : public QObject
@@ -16,35 +39,44 @@ class RADIO_DLLEXPORT RadioController : public QObject
     Q_OBJECT
 
 public:
-    RadioController();
-	//TODO remove, instead return a volume slider that is configured and ready
-	AudioPlaybackEngine* audioPlaybackEngine() const { return m_audio; }
-    
+    RadioController( Phonon::AudioOutput* );
+
+	Phonon::AudioOutput* audioOutput() const { return m_audioOutput; }
+	
 public slots:
     void play( const RadioStation& );
     void stop();
     void skip();
+	void pause();
+	void unpause();
 
 private slots:
     void enqueue( const QList<Track>& );
-
+    void onPhononStateChanged( Phonon::State, Phonon::State );
+	void onPhononAboutToFinish();
+	
 signals:
 	void tuningIn( const QString& title );
 	void tuned( const QString& title );
 	void preparing( const Track& );
-	/** buffering and percentage, you can get this mid-track too, if so, see playbackResumed() */
+	/** buffer fill status as a percentage, you can get this mid-track too, if 
+	  * so, see playbackResumed() */
 	void buffering( int );
     void trackStarted( const Track& );
 	/** stop() was called or a serious radio error occurred */
     void playbackEnded();
 	
-    /** if the buffers empty mid track, you'll get buffering */
+	void playbackPaused();
+	
+    /** follows pause and buffering */
     void playbackResumed();
 
 private:
-	Tuner* m_tuner;
-    AudioPlaybackEngine* m_audio;
-	QString m_currentStation;
+	class Tuner* m_tuner;
+	Phonon::AudioOutput* m_audioOutput;
+	Phonon::MediaObject* m_mediaObject;
+	
+    QMap<QUrl, Track> m_queue;
 };
 
 #endif //RADIOCONTROLLER_H
