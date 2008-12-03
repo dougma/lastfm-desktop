d3f7f951d4b4af8a13da0826b18930184d8fa2ff
Doug Mansell <doug.mansell@gmail.com>
2008-12-03 12:39:53 +0000

    AutoTransaction missing from previous previous commit
    git-svn-id: svn+ssh://svn.last.fm/svn/clientside/trunk/desktop@112416 ab8f4a45-97f9-0310-bbd1-854ce3dcee89

---
+++ app/clientplugins/localresolver/AutoTransaction.h
@@ -0,0 +1,59 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by                                                 *
+ *      Last.fm Ltd <client@last.fm>                                       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
+#ifndef AUTO_TRANSACTION_H
+#define AUTO_TRANSACTION_H
+
+template<typename TDatabase>
+class AutoTransaction
+{
+private:
+    TDatabase& m_db;
+    bool m_rollback;
+
+public:
+    AutoTransaction(TDatabase &db)
+        : m_db(db)
+        , m_rollback(true)
+    {
+        m_db.transactionBegin();
+    }
+
+    ~AutoTransaction()
+    {
+        if (m_rollback) {
+            m_db.transactionRollback();
+        } else {
+            m_db.transactionCommit();
+        }
+    }
+
+    void rollback()
+    {
+        m_rollback = true;
+    }
+
+    void commit()
+    {
+        m_rollback = false;
+    }
+};
+
+#endif
