daf76ff703cb24cbdf8a4d6408e13b86b60c54e9
Jono Cole <jonocole@gmail.com>
2009-02-09 17:17:56 +0000

    Show pretty metadata using webkit. Only artist image and minimal info atm..
    git-svn-id: svn+ssh://svn.last.fm/svn/clientside/trunk/desktop@119909 ab8f4a45-97f9-0310-bbd1-854ce3dcee89

--- app/client_1.6/MetaDataWidget.cpp
+++ app/client_1.6/MetaDataWidget.cpp
@@ -1,10 +1,37 @@
 #include "MetaDataWidget.h"
+#include "lib/lastfm/ws/WsReply.h"
+#include <QPalette>
 
+static const QString css = 
+    "<style>"
+    "img { "
+    "    float: left;"
+    "    display: inline;"
+    "    padding-right: 2em;"
+    "    padding-bottom: 1.5em;"
+    "}"
+    
+    "body {"
+    "    background-image: url('http://cdn.last.fm/client/images/watermark.png');"
+    "    background-repeat: no-repeat;"
+    "    background-position: right bottom;"
+    "    background-attachment: fixed;"
+    "}"
+    "h1 {"
+    "   font-size: 2em;"
+    "   margin: 0em;"
+    "   padding: 0em;"
+    "}"
+    "h2 {"
+    "   font-size:1.5em;"
+    "   margin: 0em;"
+    "   padding: 0em;"
+    "}"
+    "</style>";
 
 MetaDataWidget::MetaDataWidget( QWidget* parent )
               : QWebView( parent )
 {
-    
 }
 
 void
@@ -17,5 +44,34 @@ MetaDataWidget::setTuningIn()
 void
 MetaDataWidget::setTrack( const Track& t )
 {
-    setHtml( t.toString() );
+    WsReply* r = t.artist().getInfo();
+    connect( r, SIGNAL( finished( WsReply* )), SLOT( onInfoFetched( WsReply* )));
+    m_track = t;
 }
+
+
+void 
+MetaDataWidget::onInfoFetched( WsReply* r )
+{
+    const QString bio = r->lfm()["artist"]["bio"]["content"].text();
+
+    QStringList paras = bio.split( QRegExp( "\\r+" ));
+    
+    QString formattedBio;
+    if( !paras.empty() )
+    { 
+        paras.replaceInStrings( QRegExp( "^(.*)$" ), "<p>\\1</p>" );
+        formattedBio = paras.join( "" );    
+    }
+    
+    const QString artistImageUrl = r->lfm()["artist"]["image size=large"].text();
+
+    formattedBio.prepend( "<html><head>"+ css +"</head><body><img src='" + artistImageUrl + "' />" 
+                          "<h1>" + m_track.title()  + "</h1>"
+                          "<h2>" + tr( "by" ) + " " + r->lfm()["artist"]["name"].text() + "</h2>"
+                        );
+    formattedBio.append( "</body></html>" );
+    setHtml( formattedBio );
+}
+
+
--- app/client_1.6/MetaDataWidget.h
+++ app/client_1.6/MetaDataWidget.h
@@ -1,12 +1,22 @@
+#ifndef METADATAWIDGET_H
+#define METADATAWIDGET_H
 
 #include <QWebView>
 
-
 class MetaDataWidget : public QWebView
 {
+Q_OBJECT
 public:
     MetaDataWidget( QWidget* parent = 0 );
     
     void setTuningIn();
     void setTrack( const class Track& );
+
+protected slots:
+    void onInfoFetched( WsReply* );
+
+protected:
+    Track m_track;
 };
+
+#endif //METADATAWIDGET_H
