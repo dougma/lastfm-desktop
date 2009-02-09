e75b322979b6d6c1c9d311e80aca18731a1f810f
Doug Mansell <doug.mansell@gmail.com>
2009-02-09 15:27:21 +0000

    restore #else lines!
    git-svn-id: svn+ssh://svn.last.fm/svn/clientside/trunk/desktop@119875 ab8f4a45-97f9-0310-bbd1-854ce3dcee89

--- app/client_1.6/container.cpp
+++ app/client_1.6/container.cpp
@@ -1152,6 +1152,7 @@ Container::onAltShiftL()
     #ifdef WIN32
         // The QDesktopServices call doesn't work on Windows
         ShellExecuteW( 0, 0, (TCHAR*)UnicornCoreApplication::log().filePath().utf16(), 0, 0, SW_SHOWNORMAL );
+    #else
         QDesktopServices::openUrl( QUrl::fromLocalFile( UnicornCoreApplication::log().filePath() ) );
     #endif
 }
@@ -1163,6 +1164,7 @@ Container::onAltShiftF()
     #ifdef WIN32
         // The QDesktopServices call doesn't work on Windows
         ShellExecuteW( 0, 0, (TCHAR*)UnicornCoreApplication::log().path().utf16(), 0, 0, SW_SHOWNORMAL );
+    #else
         QDesktopServices::openUrl( QUrl::fromLocalFile( CoreDir::logs().path() ) );
     #endif
 }
