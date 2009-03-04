709003d5a021f4d42b803ca202531ceb58a691d0
Max Howell <max@methylblue.com>
2009-03-04 15:57:12 +0000

    Generate source tarball for Boffin. app specific scripts should not be in admin. No! But this is quicker for now. I'll fix it all up eventually. admin is for general purpose build/dist related scripts.
    git-svn-id: svn+ssh://svn.last.fm/svn/clientside/trunk/desktop@123141 ab8f4a45-97f9-0310-bbd1-854ce3dcee89

---
+++ admin/dist/src_tarball.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+d=Boffin-$1
+mkdir -p $d/app/client
+
+cp -R common admin lib $d
+cp app/* $d/app
+cp -R app/boffin app/clientplugins $d/app
+cp app/client/Resolver.cpp $d/app/client
+cp app/client/XspfTrackSource.cpp $d/app/client
+cp app/client/ResolvingTrackSource.cpp $d/app/client
+cp app/client/XspfResolvingTrackSource.cpp $d/app/client
+cp app/client/LocalRql.cpp $d/app/client
+cp app/client/LocalRadioTrackSource.cpp $d/app/client
+cp app/client/Resolver.h $d/app/client
+cp app/client/XspfTrackSource.h $d/app/client
+cp app/client/ResolvingTrackSource.h $d/app/client
+cp app/client/XspfResolvingTrackSource.h $d/app/client
+cp app/client/LocalRql.h $d/app/client
+cp app/client/LocalRadioTrackSource.h $d/app/client
+
+
+dirs=`find $d -name .svn`
+for x in $dirs; do ../../tools/svn-clean `dirname "$x"`; done
+echo $dirs | xargs rm -rf
+rm -r $d/lib/listener $d/lib/lastfm/fingerprint $d/lib/3rdparty
+cp COPYING configure .qmake.cache $d
+
+cat <<END>$d/Last.fm.pro
+TEMPLATE = subdirs
+CONFIG += ordered
+SUBDIRS = lib/lastfm/core/libcore.pro \
+          lib/lastfm/ws/libws.pro \
+          lib/lastfm/types/libtypes.pro \
+          lib/lastfm/radio/libradio.pro \
+          lib/unicorn/libunicorn.pro \
+          app/clientplugins/localresolver/libresolver.pro \
+          app/boffin
+END
+
+
+cat <<END>$d/README
+Dependencies
+============
+Taglib 1.5
+Qt 4.4
+sqlite3
+boost
+
+Compile Errors
+==============
+Please, I'm not a support dude! I'd appreciate it if you don't email me until 
+you're sure it's our fault it isn't compiling! Thanks. <max@last.fm>
+END
+
+tar cjf $d.tar.bz2 $d
+rm -rf $d
