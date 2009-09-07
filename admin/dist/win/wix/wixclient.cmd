14c44159c29bf9ef26c330dc1e9199b20e8043ab
dougma <doug.mansell@gmail.com>
2009-09-07 16:42:49 +0100

    whip the installer files into shape

--- admin/dist/win/wix/wixclient.cmd
+++ admin/dist/win/wix/wixclient.cmd
@@ -1,2 +1,23 @@
-"%WIX%"\bin\candle Product.wxs
-"%WIX%"\bin\light -ext WixUIExtension -cultures:en-us Product.wixobj -b ..\_bin\ -b C:\dev\Qt\4.4.3\bin
+@echo off
+rem Requires Wix 3.0.x
+rem Run with current directory same as .cmd location
+
+rem set VCREDISTDIR=C:\Program Files\Microsoft Visual Studio 8\SDK\v2.0\BootStrapper\Packages\vcredist_x86\
+set VCREDISTDIR=C:\Program Files\Common Files\Merge Modules\
+
+echo WIX = %WIX%
+echo QTDIR = %QTDIR%
+echo VCREDISTDIR = %VCREDISTDIR%
+
+"%WIX%"\bin\candle client.wxs
+if ERRORLEVEL 1 goto ERROR
+
+"%WIX%"\bin\light -ext WixUIExtension client.wixobj -b ..\..\..\..\ -b ..\..\..\..\_bin\ -b %QTDIR%\bin -b %QTDIR%\plugins -b "%VCREDISTDIR%\"
+if ERRORLEVEL 1 goto ERROR
+
+goto END
+
+:ERROR
+echo "***Fail"
+
+:END
